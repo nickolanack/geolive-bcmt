12662c3407c5468a675bf4e06b3e4cd3b2ef8cc2
nickolanack <>
2018-06-15 10:58:29 -0700

    -- Auto Commit - Changes made through Frontend Interface

---
+++ app/Placemark Infowindow.content.3.script.js
@@ -0,0 +1,8 @@
+return (new ModalFormButtonModule(item.getMap(), item, {
+    "label":"Site Comments And Notes",
+    "formName":"itemAttributesView",
+    "formOptions":{
+       // "template":"form"
+    },
+    "className":"WizardButton btn"
+}))
