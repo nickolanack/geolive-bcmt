015361991eca5d5e0fd678bfa8f0f8c70dd8be2a
nickolanack <apache@s54.ok.ubc.ca>
2018-06-17 22:36:54 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/widget.DetailView.32.content.1.script.js
+++
@@ -1,8 +0,0 @@
-return (new ModalFormButtonModule(item.getMap(), item, {
-    "label":"Site Comments And Notes",
-    "formName":"itemAttributesView",
-    "formOptions":{
-       // "template":"form"
-    },
-    "className":"field-value-module detail-link"
-}))
