ceabb6bdbd275382d72caca6f992c79bbdd5fa73
nickolanack <apache@s54.ok.ubc.ca>
2018-11-09 09:37:12 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/Google Roads Style.customScript.js
@@ -0,0 +1,22 @@
+application.getBaseMap().setOptions({
+	styles: [
+  {
+    "featureType": "poi.business",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.place_of_worship",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  }
+]
+});
