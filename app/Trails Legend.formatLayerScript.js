b66a46ae10abd63f1f4265d49a1d86320246dfa3
nickolanack <apache@s54.ok.ubc.ca>
2018-11-12 08:00:20 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Trails Legend.formatLayerScript.js
+++ app/Trails Legend.formatLayerScript.js
@@ -23,7 +23,7 @@ var getGroup=function(layer, element){
             if(id+""==map[j]+""){
                 return key;
             }
-            element.addClass("not-"+map[j]);
+            //element.addClass("not-"+map[j]);
         }
         // element.addClass("not-"+map.join('-'));
     }
@@ -36,12 +36,35 @@ var addToGroup=function(group, layer, element){
     
     var category=me._layerGroupEls[group];
     if(!category){
-        category=new Element('li');
+        category=new Element('li',{"class":"layer"});
         me._layerGroupEls[group]=category
         element.parentNode.insertBefore(category, element);
         category.appendChild(Asset.image(element.firstChild.src,{style:element.firstChild.style}));
         category.appendChild(new Element('span',{"class":"label"}));
-        category.appendChild(new Element('span',{"class":"indicator-switch"}));
+        category.appendChild(new Element('span',{"class":"indicator-switch", events:{'click':function(){
+            
+            var layers=me._layerGroupsMap[group].map(function(lid){return application.getLayerManager().getLayer();});
+            var value=false;
+            layers.forEach(function(l){
+                if(l.isVisible()){
+                    value=true;
+                }
+            });
+            layers.forEach(function(l){
+                if(value){
+                    l.hide();
+                }else{
+                    l.show();
+                }
+            });
+            
+            if(value){
+                category.removeClass('active');
+            }else{
+                category.addClass('active');
+            }
+            
+        }}}));
     }else{
         
     }
