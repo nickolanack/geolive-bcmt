f3f1f042b0db154e20b1093b8e50a508fcfb27f9
nickolanack <apache@s54.ok.ubc.ca>
2018-11-21 21:35:53 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/Esri Tile.customScript.js
@@ -0,0 +1,33 @@
+map.getBaseMap().mapTypes.set('EsriWorldMap', new google.maps.ImageMapType({
+  name: "Esri World",
+  getTileUrl: function(coord, zoom) {
+    console.log(coord);
+    var url = 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+      .replace('{x}', coord.x)
+      .replace('{y}', coord.y)
+      .replace('{z}', zoom);
+    return url;
+  },
+  tileSize: new google.maps.Size(256, 256),
+  minZoom: 1,
+  maxZoom: 20
+}));
+
+
+var last;
+
+tile.addEvent('click',{
+    
+    
+    if(map.getBaseMap().getMapTypeId()=='EsriWorldMap'){
+         map.getBaseMap().setMapTypeId(last);
+    }else{
+        last=map.getBaseMap().getMapTypeId();
+         map.getBaseMap().setMapTypeId('EsriWorldMap');
+    }
+   
+    
+    
+    
+    
+});
